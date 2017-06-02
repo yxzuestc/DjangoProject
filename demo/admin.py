089b6508aabe36375aed414952507e4a2729cb5a
xingzhong <yxz_uestc@163.com>
2017-06-02 22:22:21 +0800

    完成第二部分练习。

--- demo/admin.py
+++ demo/admin.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from .models import Question, Choice
 from django.contrib import admin
 
 # Register your models here.
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Infomation', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]    # 在question中显示choice
+    list_display = ('question_text', 'pub_date', 'was_published_recently')  #显示的字段
+    list_filter = ['pub_date']      #过滤器
+    search_fields = ['question_text']   #搜索
+
+
+admin.site.register(Question, QuestionAdmin)
